e42166e3829ccdd13c8c4d29230cba7f54ca892d
Jukem <chris@dixn.dev>
2023-10-16 14:25:18 -0700

    fixed invalid date bug

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post('/api/users', (req, res) => {
 
 app.post('/api/users/:_id/exercises', (req, res) => {        //TODO
   const user = Database.find(x => x._id == req.params._id)
-  const date = new Date(req.body.date) || new Date()
+  const date = req.body.date ? new Date(req.body.date) : new Date()
   console.log(req.body.date, date)
   user.log.push({description: req.body.description, duration: +req.body.duration, date: date.toDateString()})
   user.count++

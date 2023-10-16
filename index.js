23e72bb655bb479670bb7433b762381e494cb8f7
Jukem <chris@dixn.dev>
2023-10-16 14:21:15 -0700

    date error checking

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.post('/api/users', (req, res) => {
 app.post('/api/users/:_id/exercises', (req, res) => {        //TODO
   const user = Database.find(x => x._id == req.params._id)
   const date = new Date(req.body.date) || new Date()
+  console.log(req.body.date, date)
   user.log.push({description: req.body.description, duration: +req.body.duration, date: date.toDateString()})
   user.count++
   res.json({username: user.username, description: req.body.description, duration: +req.body.duration, date: date.toDateString(), _id: user._id})
